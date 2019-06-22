83bb48efc75eeea0c41682c00610426314eaba8e
jsonlog <jsonlogit@gmail.com>
2019-06-22 16:49:28 +0800

    https://github.com/linkedin/pygradle

---
+++ mypthon/python.py
@@ -0,0 +1,15 @@
+import sys
+# comment
+"""
+comment
+comment2
+
+"""
+print('Python %s on %s' %(sys.version,sys.platform))
+
+hello="Hello world"
+print(hello)
+print("2的3次方=",2**3)
+print("raw_input your name")
+name = input()
+print("your name",name)
