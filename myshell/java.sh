9b4fb4a7cefdba17708eb887aaa285610f15ae89
jsonlog <jsonlogit@gmail.com>
2019-06-30 13:54:29 +0800

    javac com/util/MDtable.java java com.util.MDtable

--- myshell/java.sh
+++ myshell/java.sh
@@ -8,7 +8,13 @@ fi
 # javac hello.java staticClass.java
 # java hello
 
-javac com/util/hello.java
-java com.util.hello
+if [ -n "$1" ];then
+    echo $1
+    javac com/util/MDtable.java
+    java com.util.MDtable
+else
+    javac com/util/hello.java
+    java com.util.hello
+fi
 
 cd -;
