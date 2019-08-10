47b91733af504129bc1748cd5c419103072fefc8
jsonlog <jsonlogit@gmail.com>
2019-08-10 14:13:57 +0800

    myspringboot

--- myspringboot/src/main/java/com/example/Example.java
+++ myspringboot/src/main/java/com/example/Example.java
@@ -2,14 +2,22 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @SpringBootApplication
 public class Example {
 
 	@RequestMapping("/")
+	String index() {
+		return "thymeleaf";
+	}
+
+	@ResponseBody
+	@RequestMapping("/home")
 	String home() {
 		return "Hello World!";
 	}
