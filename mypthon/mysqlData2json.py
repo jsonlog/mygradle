ef912d178d4cad51a64f8578d74f9996dc8aa244
jsonlog <jsonlogit@gmail.com>
2020-06-20 18:55:05 +0800

    mysqlTojson

--- mypthon/mysqlData2json.py
+++ mypthon/mysqlData2json.py
@@ -5,14 +5,24 @@ from __future__ import division
 from __future__ import print_function
 import json
 import pymysql
+import datetime
 
+# datetimeTypeError: datetime.datetime(2017, 3, 21, 2, 11, 21) is not JSON serializable。
+class CJsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.strftime('%Y-%m-%d %H:%M:%S')
+        elif isinstance(obj, date):
+            return obj.strftime("%Y-%m-%d")
+        else:
+            return json.JSONEncoder.default(self, obj)
 
 # 连接数据库
 conn = pymysql.connect(
             host='localhost',
             user='root',
-            passwd='1234',
-            db='testd2rq',
+            passwd='123456',
+            db='life',
             charset='utf8',
             use_unicode=True
         )
@@ -20,7 +30,7 @@ conn = pymysql.connect(
 cursor = conn.cursor()
 
 # 执行查
-cursor.execute("SELECT * FROM user")
+cursor.execute("SELECT * FROM what")
 # 查询数据库多条数据
 result = cursor.fetchall()
 # for data in result:
@@ -42,11 +52,15 @@ column_list = []
 for i in fields:
     # 提取字段名，追加到列表中
     column_list.append(i[0])
-# print(column_list)
+    # print('"\n    },\n    {\n      "field": "',end="")
+    # print(i[0],end="")
+    # print('",\n      "title": "',end="")
+    # print(i[0],end="")
+print(column_list)
 # ['Id', 'name', 'password', 'birthplace']
 
 # 打开输出结果文件
-with open('../data/json.txt', 'w+') as f:
+with open('json.data', 'w+') as f:
     # 一次循环，row代表一行，row以元组的形式显示
     for row in result:
         # 定义Python 字典
@@ -54,13 +68,14 @@ with open('../data/json.txt', 'w+') as f:
         # 将row中的每个元素，追加到字典中。
         for i in range(len(column_list)):
             data[column_list[i]] = row[i]
+        # print(data)
         # data[column_list[0]] = row[0]
         # # Python字段格式 和json字段格式转换
         # data[column_list[1]] = str(row[1])
         # data[column_list[2]] = str(row[2])
         # data[column_list[3]] = str(row[3])
         # Python的dict --转换成----> json的object
-        jsondata = json.dumps(data, ensure_ascii=False)
+        jsondata = json.dumps(data, cls=CJsonEncoder) #,ensure_ascii=False)
         # 写入文件
-        f.write(jsondata + '\n')
-        f.close()
+        f.write(jsondata + ',\n')
+f.close()
