562fef4a1ae1abb4cac2e53aab58d3cb006dd17b
jsonlog <jsonlogit@gmail.com>
2020-08-19 21:51:37 +0800

    php

---
+++ myphp/hello/school.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+<?php
+// 这是 PHP 单行注释
+
+/*
+这是
+PHP 多行
+注释
+*/
+?>
+<?php
+$x=5;
+$y=6;
+$z=$x+$y;
+echo $z;
+?>
+</body>
+</html>
